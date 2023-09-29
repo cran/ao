00c3cc7389f78f49b739222e72248e5ca4fc6c22
Lennart Oelschläger <oelschlaeger.lennart@gmail.com>
2023-09-29 08:30:02 +0000

    version 0.2.7

---
+++ R/ao-package.R
@@ -0,0 +1,9 @@
+#' @aliases ao-package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom optimizeR optimizer_optim
+#' @importFrom optimizeR apply_optimizer
+## usethis namespace: end
+NULL
--- inst/doc/ao.R
+++ inst/doc/ao.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -8,6 +8,8 @@ knitr::opts_chunk$set(
 ## ----ao-----------------------------------------------------------------------
 library("ao")
 himmelblau <- function(x) (x[1]^2 + x[2] - 11)^2 + (x[1] + x[2]^2 - 7)^2
-ao(f = himmelblau, p = c(0,0), partition = list(1, 2),
-   base_optimizer = optimizer_optim(lower = -5, upper = 5, method = "L-BFGS-B"))
+ao(
+  f = himmelblau, p = c(0, 0), partition = list(1, 2),
+  base_optimizer = optimizer_optim(lower = -5, upper = 5, method = "L-BFGS-B")
+)
 
